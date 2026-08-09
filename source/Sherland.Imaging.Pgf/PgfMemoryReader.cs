5474e7813c94065bdc3663fbdf2bc1134212c2c8
Steinar Herland <steinar.herland@gmail.com>
2026-08-09 09:15:47 +0200

    Fix missing xeraina copyright on two direct-port files
    PgfMemoryReader.cs (a line-by-line translation of CPGFMemoryStream in
    PGFstream.cpp) and PgfConstants.cs (constants mirrored directly from
    PGFtypes.h/PGFplatform.h/WaveletTransform.h) were classified as
    Steinar-Herland-only during the Aug 7 SPDX header pass, inconsistent
    with every other direct-port file in this directory.
    
    Fixes #3.

--- source/Sherland.Imaging.Pgf/PgfMemoryReader.cs
+++ source/Sherland.Imaging.Pgf/PgfMemoryReader.cs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
-// Copyright (C) 2026 Steinar Herland.
+// Copyright (C) 2006 xeraina GmbH. Portions Copyright (C) 2026 Steinar Herland.
 
 namespace Sherland.Imaging.Pgf;
 
